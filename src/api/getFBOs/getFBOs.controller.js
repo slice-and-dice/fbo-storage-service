77d56bafd173a20970519c68349a97f7a344c7c4
Django Shelton <djangoshelton@gmail.com>
2018-04-25 11:19:00 +0100

    added logging

--- src/api/getFBOs/getFBOs.controller.js
+++ src/api/getFBOs/getFBOs.controller.js
@@ -1,12 +1,16 @@
 const FBO = require('../../db/models/FBO');
 
 const getAllFBOs = async () => {
+  winston.info('get all fbo controller called');
   const FBOs = await FBO.findAll();
+  winston.info('get all fbo controller successful');
   return FBOs;
 }
 
 const getFBOByID = async (id) => {
+  winston.info('get all fbo by id controller called');
   const response = await FBO.findOne({ where: {fsa_rn: id} });
+  winston.info('get all fbo by id controller successful');
   return response;
 }
 
--- src/api/getFBOs/getFBOs.router.js
+++ src/api/getFBOs/getFBOs.router.js
@@ -5,10 +5,12 @@ module.exports = () => {
   const router = Router();
 
   router.get('', async (req, res) => {
+    winston.info('get all fbo router called');
     res.send(await getFBOsController.getAllFBOs());
   });
 
   router.get('/:id', async (req, res) => {
+    winston.info('get fbo by ID router called');
     res.send(await getFBOsController.getFBOByID(req.params.id));
   });
 
